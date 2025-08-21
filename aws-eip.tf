5ee7f96fe032d366895c169abe74193f617b0157
git-bharat <144121815+git-bharat@users.noreply.github.com>
2025-08-21 11:49:26 +0000

    updated

--- aws-eip.tf
+++ aws-eip.tf
@@ -1,4 +1,4 @@
-resource "aws_eip" "citadel_eip" {
+resource "aws_eip" "eip" {
   vpc = true
   instance = aws_instance.citadel.id
   # Local-exec provisioner to save the public_dns to a file
@@ -6,7 +6,4 @@ resource "aws_eip" "citadel_eip" {
     command = "echo ${self.public_dns} > /root/citadel_public_dns.txt"
         }
 }
-resource "aws_eip_association" "citadel_eip_assoc" {
-  instance_id   = aws_instance.citadel.id
-  allocation_id = aws_eip.citadel_eip.id
-}
+
